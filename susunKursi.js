8dbcee63cfbb521a7625a39e0879982d9e340536
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-10-30 11:05:27 +0700

    done

--- susunKursi.js
+++ susunKursi.js
@@ -9,34 +9,33 @@
 // Baris 2 :  [ 'c', 'f', 'i' ]
 
 'use strict'
-
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for (let i = 0; i < row; i++) {
+		seats.push([])
 	}
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats)
 	let row = 0
-
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+	for (let i = 0; i < arr.length; i++) {
+		seats[row].push(arr[i])
+		if (row < 2) {
 			row++
-		} else {
-			row == 0
+		}
+		else {
+			row = 0
 		}
 	}
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for (let i = 0; i < seats.length; i++) {
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
 
-managePerson(['a','b','c','d','e','f','g','h','i','j'], 3)
+managePerson(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], 3)
