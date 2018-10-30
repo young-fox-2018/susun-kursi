3feda2baba311dbdc573a78fa69bd2bb689ed687
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-10-30 11:02:27 +0700

    submitting this ex

--- susunKursi.js
+++ susunKursi.js
@@ -12,30 +12,35 @@
 
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i=0; i<row; i++) {
+		seats.push([])
 	}
 	return seats
 }
 
-function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
-	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+function managePerson(arr, rowSeats) {
+	let seats = generateSeats(rowSeats)
+	let row = 0
+	debugger
+
+	for(let i=0; i<arr.length; i++) {
+		// console.log(seats[row])
+		// console.log(arr[i])
+		seats[row].push(arr[i])
+		if(row < 2) {
 			row++
 		} else {
-			row == 0
+			row = 0
 		}
+		debugger
 	}
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
-		console.log(`Baris ${i} : `, seats[i])
+	for(let i=0; i<seats.length; i++) {
+		console.log(`Baris ${i+1} : `, seats[i])
 	}
 }
 
