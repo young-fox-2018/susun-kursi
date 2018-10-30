fd2ffc3adca562d7977c76f48d8353ff441ad6f4
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-10-30 11:06:48 +0700

    susun kursi

--- susunKursi.js
+++ susunKursi.js
@@ -8,33 +8,34 @@
 // Baris 1 :  [ 'b', 'e', 'h' ]
 // Baris 2 :  [ 'c', 'f', 'i' ]
 
-'use strict'
+// 'use strict'
 
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i=0; i<=row; i++) {
+		seats.push([])
 	}
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats)
 	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+	for(let i=0; i<arr.length; i++) {
+		seats[row].push(arr[i])
+		if(row < rowSeats-1) {
 			row++
 		} else {
-			row == 0
+			row = 0
+			// i == 0
 		}
 	}
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for(let i=0; i<seats.length-1; i++) {
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
