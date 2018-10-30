f070c64a3c58c37d50741f5749505dfef9113464
DzikryFathullah <dzizee.fathullah@gmail.com>
2018-10-30 22:23:49 +0700

    susun kursi done

--- susunKursi.js
+++ susunKursi.js
@@ -12,31 +12,32 @@
 
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i=0; i<row; i++) {
+		seats.push([])
 	}
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	debugger
+	let seats = generateSeats(rowSeats)
 	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+	for(let i = 0; i < arr.length ; i++) {
+		seats[row].push(arr[i])
+		if(row < 2) {
 			row++
 		} else {
-			row == 0
+			row = 0
 		}
 	}
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for(let i=0; i<seats.length; i++) {
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
-
+debugger
 managePerson(['a','b','c','d','e','f','g','h','i','j'], 3)
